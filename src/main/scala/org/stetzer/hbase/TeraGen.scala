c7b7bf8d25a735882d22d9ebeec382b165914431
Mark Stetzer <stetzer@gmail.com>
2013-10-16 18:18:01 -0400

    Using Executors for now

--- src/main/scala/org/stetzer/hbase/TeraGen.scala
+++ src/main/scala/org/stetzer/hbase/TeraGen.scala
@@ -1,32 +1,48 @@
 package org.stetzer.hbase
 
+import java.util.concurrent.{TimeUnit, Executors}
+
+import org.apache.commons.lang.{RandomStringUtils => R}
+
+import org.apache.hadoop.conf.Configuration
+
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.client.{Put, HTable}
 import org.apache.hadoop.hbase.util.Bytes.{toBytes => B}
 
-import org.apache.commons.lang.{RandomStringUtils => R}
 
 object TeraGen {
   def main(args: Array[String]) = {
-    if(args.length < 1) {
-      System.err.println("Usage: TeraGen <number of records> <table name>")
+    if(args.length < 3) {
+      System.err.println("Usage: TeraGen <number of records> <table name> <number of concurrent Puts>")
       System.exit(1)
     }
 
     val totalRecords = args(0).toInt
     val tableName = args(1)
+    val concurrentPuts = args(2).toInt
+
+    val exec = Executors.newFixedThreadPool(concurrentPuts)
 
     val config = HBaseConfiguration.create()
-    val table = new HTable(config, tableName)
 
     val startTime = System.currentTimeMillis()
     for(i <- 1 to totalRecords) {
-      val p = new Put(B(R.randomAlphanumeric(10)))
-      p.add(B("colfam"), B("col"), B(R.randomAlphanumeric(250)))
-
-      table.put(p)
+      exec.execute(new RunnablePut(config, tableName))
     }
 
+    exec.shutdown()
+    exec.awaitTermination(5, TimeUnit.MINUTES)
+
     println(s"Total records put: $totalRecords in ${System.currentTimeMillis() - startTime} ms")
+  }
+}
+
+class RunnablePut(conf:Configuration, tableName:String) extends Runnable {
+  def run() {
+    val p = new Put(B(R.randomAlphanumeric(10)))
+    p.add(B("colfam"), B("col"), B(R.randomAlphanumeric(250)))
+
+    new HTable(conf, tableName).put(p)
   }
 }
